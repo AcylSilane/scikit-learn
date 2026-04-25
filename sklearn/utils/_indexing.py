a58fe424fa29f15db979f0a9606ccc73b114f733
James Dean <24254612+AcylSilane@users.noreply.github.com>
2026-04-25 19:32:10 -0400

    fix ruff linting issue

--- sklearn/utils/_indexing.py
+++ sklearn/utils/_indexing.py
@@ -473,8 +473,7 @@ def _get_column_indices(X, key):
                 col_idx = all_columns.get_loc(col)
                 if not isinstance(col_idx, numbers.Integral):
                     raise ValueError(
-                        f"Selected columns, {columns}, "
-                        "are not unique in dataframe"
+                        f"Selected columns, {columns}, are not unique in dataframe"
                     )
                 column_indices.append(col_idx)
 
